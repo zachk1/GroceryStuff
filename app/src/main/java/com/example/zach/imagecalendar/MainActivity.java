5f35add8074c83c8fb0250be317146d08c8e6bed
zachk1 <zachk1@vt.edu>
2017-10-07 16:05:42 -0400

    Adds code and button for calendar

--- app/src/main/java/com/example/zach/imagecalendar/MainActivity.java
+++ app/src/main/java/com/example/zach/imagecalendar/MainActivity.java
@@ -15,6 +15,13 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import java.io.File;
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.CalendarContract;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 
 
@@ -22,7 +29,18 @@ public class MainActivity extends Activity {
 
     Button button;
     ImageView imageView;
+    Button calendarbutton;
     static final int REQUEST_IMAGE_CAPTURE = 1;
+    String title = "SuperCoolStuff";
+    String location = "SuperCoolPlace";
+    String description = "Super Cool";
+    String year = "2017";
+    String month = "December";
+    String day = "7";
+    String time = "2300";
+
+
+
 
 
 
@@ -31,6 +49,7 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         button = (Button) findViewById(R.id.button);
+        calendarbutton = (Button) findViewById(R.id.calendarbutton);
         imageView = (ImageView) findViewById(R.id.window);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,6 +60,23 @@ public class MainActivity extends Activity {
                 startActivityForResult(camera_intent, REQUEST_IMAGE_CAPTURE);
             }
         });
+        calendarbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent calendarIntent = new Intent(Intent.ACTION_INSERT, CalendarContract.Events.CONTENT_URI);
+                calendarIntent.setType("vnd.android.cursor.item/event");
+                calendarIntent.putExtra(CalendarContract.Events.TITLE, "Lit Party");
+                calendarIntent.putExtra(CalendarContract.Events.EVENT_LOCATION, "Pritchard");
+                calendarIntent.putExtra(CalendarContract.Events.DESCRIPTION, "BBQ");
+
+                GregorianCalendar calDate = new GregorianCalendar(2017, 10, 8);
+                calendarIntent.putExtra(CalendarContract.EXTRA_EVENT_ALL_DAY, true);
+                calendarIntent.putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, calDate.getTimeInMillis());
+                calendarIntent.putExtra(CalendarContract.EXTRA_EVENT_END_TIME, calDate.getTimeInMillis());
+                startActivity(calendarIntent);
+            }
+        });
+
     }
 
 
@@ -51,10 +87,10 @@ public class MainActivity extends Activity {
         return image_file;
     }
 
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         String path = "sdcard/image_calendar/cam_image.jpg";
         imageView.setImageDrawable(Drawable.createFromPath(path));
     }
 }
-
